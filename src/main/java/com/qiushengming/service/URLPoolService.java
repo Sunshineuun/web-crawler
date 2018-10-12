bddab2cf7670fca88d3d60e63f0e5936090bdc1e
qiushengming@aliyun.com <sunshine>
2018-10-12 13:57:34 +0800

    优化

--- src/main/java/com/qiushengming/service/URLPoolService.java
+++ src/main/java/com/qiushengming/service/URLPoolService.java
@@ -8,10 +8,10 @@ public interface URLPoolService extends ManagementService<URL>{
 
   /**
    * 查询资源池， 建议增量获取，不要一次获取很多
-   *
+   * @param type 类型
    * @return {@link URL}集合
    */
-  URL getURL();
+  URL getURL(String type);
 
   void updateSuccess(URL url);
 }
