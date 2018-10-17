e7e327236298a61cdf715713ddc1f6c819bc99a4
qiushengming@aliyun.com <sunshine>
2018-10-17 11:01:41 +0800

    优化

--- src/main/java/com/qiushengming/business/Medlive.java
+++ src/main/java/com/qiushengming/business/Medlive.java
@@ -205,15 +205,17 @@ public class Medlive extends BaseWebCrawler {
       datas.add(map);
     }
 
-    //数据 to Excel
-    SXSSFWorkbook wb = DataToExecl.createSXSSFWorkbook();
-    Sheet sheet = DataToExecl.createSheet(wb);
-    DataToExecl.writeData(Arrays.asList(titles), datas, sheet);
+    if(!datas.isEmpty()){
+      //数据 to Excel
+      SXSSFWorkbook wb = DataToExecl.createSXSSFWorkbook();
+      Sheet sheet = DataToExecl.createSheet(wb);
+      DataToExecl.writeData(Arrays.asList(titles), datas, sheet);
 
-    File file = new File(String.format("%s/%s_%s.xlsx", TEMP_PATH, getSiteName(), DateUtils
-        .nowDate()));
-    wb.write(new FileOutputStream(file));
-    getEmailTool().sendSimpleMail(getSiteName(), file);
+      File file = new File(String.format("%s/%s_%s.xlsx", TEMP_PATH, getSiteName(), DateUtils
+          .nowDate()));
+      wb.write(new FileOutputStream(file));
+      getEmailTool().sendSimpleMail(getSiteName(), file);
+    }
   }
 
   @Override
