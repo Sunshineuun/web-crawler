b6400bda79ef5d91b31d3a490aa048ac5b492809
qiushengming@aliyun.com <sunshine>
2018-10-31 16:18:31 +0800

    优化

--- src/main/java/com/qiushengming/business/Nhfpc.java
+++ src/main/java/com/qiushengming/business/Nhfpc.java
@@ -30,7 +30,7 @@ public class Nhfpc extends Medlive{
    */
   @Async
   @Override
-  @Scheduled(cron = "0 0/1 * * * ?")
+  @Scheduled(cron = "0 0 0 1 * ? ")
   public void start() {
     super.start();
   }
